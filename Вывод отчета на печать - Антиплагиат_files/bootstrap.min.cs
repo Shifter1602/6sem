309ce19c211aa899111d1d54985753f2a8ccac50
Shifter1602 <younkinpeter1602@mail.ru>
2017-06-17 06:55:24 +0300

    GPSS

---
+++ Вывод отчета на печать - Антиплагиат_files/bootstrap.min.cs
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Антиплагиат</title>
+
+    <link rel="shortcut icon" href="/img/icons/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="/img/icons/favicon.ico" type="image/x-icon" />
+    
+    <link type="text/css" href="/js/bootstrap/2.2.2/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/css/main.css" rel="stylesheet">
+    
+    <script src="/js/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script src="/js/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+
+    <script src="/js/bootstrap/2.2.2/js/bootstrap.min.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="ap-top-line">
+
+            <div class="ap-top-logo">
+                <a href="/" class="brand" title="">
+                    <img src="/img/logo/logo.png" alt="" />
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container center" style="font-size: 30px; padding: 0px 0px 20px 0px">Ошибка</div>
+    
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date(); a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-49995109-2', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+    <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function () { try { w.yaCounter26019309 = new Ya.Metrika({ id: 26019309, clickmap: true, trackLinks: true, accurateTrackBounce: true }); } catch (e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/26019309" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+
+</body>
+</html>
+
